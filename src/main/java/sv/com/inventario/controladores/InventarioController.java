5823b640faee2c35ff54dca7803c727ae9165c38
irvinldt <irvinldt@gmail.com>
2018-06-07 09:12:57 -0600

    make it better

--- src/main/java/sv/com/inventario/controladores/InventarioController.java
+++ src/main/java/sv/com/inventario/controladores/InventarioController.java
@@ -24,6 +24,7 @@ public class InventarioController {
     @GetMapping(value = "/")
     public ResponseEntity<?> consTodo() {
         try {
+        	
             return new ResponseEntity<>(inventariodtoService.consTodo(), HttpStatus.OK);
         }
         catch (Exception ex) {
